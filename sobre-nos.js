49a652f6277d95f86a6814fe896045b0706a68b9
Guilherme <guilhermehenrike0412@gmail.com>
2025-11-25 13:00:45 -0300

    adicionei o header

---
+++ sobre-nos.js
@@ -0,0 +1,15 @@
+document.getElementById("header").innerHTML = `
+    <div class="cabecalho">
+        <img src="imagens/logo.png" alt="" id="logo">
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="" id="adotar"><strong>Adotar</strong></a></li>
+                    <li><a href=""><strong>Sobre nós</strong></a></li>
+                    <li><a href=""><strong>Doação</strong></a></li>
+                    <li><a href=""><strong>Fale conosco</strong></a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+`;
