e824dcd0603d314c3a37cba6e4c57d529865a886
Guilherme <guilhermehenrike0412@gmail.com>
2025-11-25 13:32:18 -0300

    ajustes de rota

--- sobre-nos.js
+++ sobre-nos.js
@@ -5,9 +5,9 @@ document.getElementById("header").innerHTML = `
             <nav>
                 <ul>
                     <li><a href="" id="adotar"><strong>Adotar</strong></a></li>
-                    <li><a href=""><strong>Sobre nós</strong></a></li>
+                    <li><a href="sobre-nos.html"><strong>Sobre nós</strong></a></li>
                     <li><a href=""><strong>Doação</strong></a></li>
-                    <li><a href=""><strong>Fale conosco</strong></a></li>
+                    <li><a href="fale-conosco.html"><strong>Fale conosco</strong></a></li>
                 </ul>
             </nav>
         </header>
